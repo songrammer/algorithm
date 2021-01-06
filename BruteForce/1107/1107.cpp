653edfcfab5ded3a1e07c025504bb4bc797a7bef
song <johyesong8686@gmail.com>
2021-01-06 17:05:27 +0900

    refactor: 1107 (리모컨) 문제 풀이 수정 [o]

--- BruteForce/1107/1107.cpp
+++ BruteForce/1107/1107.cpp
@@ -6,54 +6,33 @@ using namespace std;
 string n;
 int m;
 int ans = 0;
-bool remote[10];
+bool remote[11];
 
-void go(int index, int cnt, string value)
-{
-    cout << index << " " << value << " : " << cnt << '\n';
+int chekPosiible(int num){//리모콘으로 갈 수 있는지 확인
 
-    if (index == n.length())
-    {
-        ans = min(ans, abs(stoi(value) - stoi(n)) + cnt);
-        cout << "end _result :" << abs(stoi(value) - stoi(n)) + cnt << '\n';
-        return;
-    }
-
-    int x = n[index] - '0';
-    cout << "pass_1" << '\n';
-
-    while (x >= 0)
-    {
-
-        if (!remote[x])
-        {
-            cout << "is there " << x << '\n';
-            string newValue = "";
-            newValue += value;
-            newValue += (x + '0');
-
-            go(index + 1, cnt + 1, newValue);
-            break;
+    int  cnt =0;
+    if(num==0){ //숫자 자체가 0일때 예외 처리
+		if(remote[num]){
+			return 0;
+		}else{
+			return 1;
+		}
+	
+	}
+	
+    while(num){
+        
+        if(remote[num%10]){
+            return 0;
+        }else{
+            num/=10;
+            cnt++;
         }
-        x--;
+       
     }
-    x = n[index] - '0';
-    cout << "pass_2" << '\n';
-    while (x <= 9)
-    {
-        if (!remote[x])
-        {
-            cout << "is there " << x << '\n';
-            string newValue = "";
-            newValue += value;
-            newValue += (x + '0');
-            go(index + 1, cnt + 1, newValue);
-            break;
-        }
-        x++;
-    }
+    return cnt;
+    
 }
-
 int main()
 {
 
@@ -66,10 +45,19 @@ int main()
         cin >> x;
         remote[x] = true;
     }
-
+   
     ans = abs(stoi(n) - 100); //수동으로 다 시도해볼때,
 
-    go(0, 0, "");
+   for(int i=0; i<=1000000;i++){
+       
+       int check=chekPosiible(i);
+       
+       if(check){
+           //갈수 있으면 이동한 다음 카운트
+           ans=min(ans,abs(stoi(n)-i)+check);
+       }
+       
+   }
 
     cout << ans << '\n';
 
