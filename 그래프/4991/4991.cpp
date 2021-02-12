8610cbcd42c1172b741a48df0c7550a0a15b9086
song <johyesong8686@gmail.com>
2021-02-12 23:25:23 +0900

    feat: 4991(로봇 청소기) 문제 풀이[x]

--- 그래프/4991/4991.cpp
+++ 그래프/4991/4991.cpp
@@ -32,34 +32,49 @@ int main() {
             }
          
         }
+        
         queue <Point> q;
         q.push({s_x,s_y,0});
         int visited[h+1][w+1];
         memset(visited,-1,sizeof(visited));
         visited[s_x][s_y]=1;
         int ans=0;
-        cout <<"먼지 :"<<cnt <<'\n';
         while(!q.empty()){
             int x=q.front().x;
             int y=q.front().y;
             int c=q.front().c;
+            q.pop();
+            if(cnt==0){
+                             
+                 break;
+            }
+            // cout<<"cur :" <<"x :"<< x <<" y :"<<y<<" time :"<<c<<'\n';
             for(int k=0; k<4;k++){
                 int nx=x+d_x[k];
                 int ny=y+d_y[k];
-                if( nx>=0 && nx <h &&ny>=0 && ny<w){
+                 
+                if(nx>=0 && nx <h &&ny>=0 && ny<w){
                     if(visited[nx][ny]==-1){
                         if(p[nx][ny]=='x') continue;
                         else if(p[nx][ny]=='*'){
+                            // cout<<"find :" <<"nx :"<< nx <<" ny :"<<ny<<" time :"<<c+1<<'\n';
                             p[nx][ny]='.';
                             cnt-=1;
                             memset(visited,-1,sizeof(visited));
-                            if(cnt){
+                            
+                            queue <Point> temp;
+                            temp.push({nx,ny,c+1});
+                            q=temp;
+                            
+                            if(cnt==0){
                                 ans=c+1;
-                                break;
                             }
-                           
+                            break;
+                        }else{
+                            //  cout<<"nx :"<< nx <<" ny :"<<ny<<" time :"<<c+1<<'\n';
+                             q.push({nx,ny,c+1});
                         }
-                         q.push({nx,ny,c+1});
+                        
                          visited[nx][ny]=1;
                     }
                 }
@@ -69,8 +84,9 @@ int main() {
         if(cnt!=0){
             cout<<-1<<'\n';
         }else{
-            cout<<ans<<'\n';
+            cout<<ans<<"\n";
         }
+        
         
     }
   
