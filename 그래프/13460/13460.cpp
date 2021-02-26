19faf9395b00ad3f6fa46e34e0d52080f6297e94
song <johyesong8686@gmail.com>
2021-02-27 02:36:21 +0900

    feat:13460(구슬 탈출 2) 문제풀이 추가 [x]

--- 그래프/13460/13460.cpp
+++ 그래프/13460/13460.cpp
@@ -2,23 +2,18 @@
 #include <queue>
 #include <cstring>
 using namespace std;
-
 int n, m;
- 
-int d_x[]={0,-1,0,1};
-int d_y[]={-1,0,+1,0};
-
+int d_x[]= {
+	0,-1,0,1
+}
+;
+int d_y[]= {
+	-1,0,+1,0
+}
+;
+int ans=-1;
 string p[15];
-
-
-struct Point{
-    
-    int x;
-    int y;
-    string route;
-    
-};
-
+int visited[15][15];
 
 bool check(int x, int y) {
 	//범위에서 벗어나는지 체크
@@ -29,133 +24,74 @@ bool check(int x, int y) {
 	}
 }
 
+void dfs(int x ,int y ,int z, int l,int cnt,int pre) {
+	if(cnt==10||p[z][l]=='O') {
+		return;
+	}
+	if(p[x][y]=='O'&&p[z][l]!='O') {
+		if(ans==-1 ||ans >cnt){
+		    ans=cnt;
+		}
+		return ;
+	}
+	for (int i=0; i<4;i++) {
+		if(i==pre) continue;
+		int nrx=x;
+		int nry=y;
+		int nbx=z;
+		int nby=l;
+		
+		for (int j=1; j<=10;j++) {
+		    int a=x+d_x[i]*j;
+		    int b=y+d_y[i]*j;
+		    int c=z+d_x[i]*j;
+		    int d=l+d_y[i]*j;
+			if(check(a,b) ==false ||  check(c,d)==false ) break;
+			
+			if(p[x+d_x[i]*j][y+d_y[i]*j]=='#') {
+				dfs(nrx,nry,nbx,nby,cnt+1,i);
+				break;
+			} else {
+				nrx=x+d_x[i]*j;
+				nry=y+d_y[i]*j;
+			}
+			if(p[z+d_x[i]*j][l+d_y[i]*j]!='#') {
+				nbx=z+d_x[i]*j;
+				nby=l+d_y[i]*j;
+			}
+			if(p[nrx][nry]=='.'&&p[nbx][nby]=='.') continue;
+			if(p[nbx][nby]=='O'||p[nrx][nry]=='O') {
+				dfs(nrx,nry,nbx,nby,cnt+1,i);
+				break;
+			}
+		}
+	}
+}
 int main() {
     
-    int r_x;
-    int r_y;
-    
-    
-    int b_x;
-    int b_y;
-    
-    cin >> n >> m;
-    
-    
-    for(int i=0; i<n;i++){
-        cin >>p[i];
-        for(int j=0; j<m;j++){
-            
-            if(p[i][j]=='R'){
-                r_x=i;
-                r_y=j;
-                p[i][j]='.';
-                
-            }else if(p[i][j]=='B'){
-                 p[i][j]='.';
-            }
-            
-        }
-    }
-    
- 
-    queue <Point> q;
-    q.push({r_x,r_y,""});
-    int visited[15][15];
-    
-    memset(visited,-1,sizeof(visited));
-    visited[r_x][r_y]=1;
-    
-    
-   
-    
-    vector <string > routes;
-     
-    while(!q.empty()){
-        
-        int x=q.front().x;
-        int y=q.front().y;
-        string route=q.front().route;
-        q.pop();
-        cout <<"x:"<<x <<" y:"<<y<<" route"<<route<<'\n';
-        
-        if(p[x][y]=='O'){
-            if(route.length()<=10){
-                cout <<"finished"<<'\n';
-                cout <<x<< " "<<y <<" arrive_route:"<<route<<'\n';
-                routes.push_back(route);
-            }
-            continue;
-           
-            
-        }
-        
-        for(int k=0; k<4;k++){
-            
-            int nx=x+d_x[k];
-            int ny=y+d_y[k];
-            
-            if(check(nx,ny)&&visited[nx][ny]==-1){
-                
-                if(p[nx][ny]=='.'||p[nx][ny]=='O'){
-                    
-                    if(route[route.length()-1]==k+'0'){
-                        q.push({nx,ny,route});
-                    }else{
-                        string next_route=route;
-                         q.push({nx,ny,next_route+=k+'0'});//다를때, 경로추가
-                    }
-                    if(p[nx][ny]!='O'){
-                         visited[nx][ny]=1;
-                    }
-                   
-                }
-            }
-            
-        }
-     
-    }
-    
-    
-    
-    bool check=false;
-    for(int i=0; i<routes.size();i++){
-        string now=routes[i];
-        int start_x=b_x;
-        int start_y=b_y;
-        check=false;
-        for(int j=0; j<now.length();j++){
-            while(true){
-                cout <<"move"<<start_x<<" ,"<< start_y<<'\n';
-                if(p[start_x][start_y]=='#'){
-                    break;
-                }else if(p[start_x][start_y]=='O'){
-                    check=true;
-                    break;
-                    
-                }
-                start_x+=d_x[now[j]-'0'];
-                start_y+=d_y[now[j]-'0'];
-                  
-            }
-            if(check){
-                break;
-            }
-            
-        }
-      
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-    return 0;
+	int r_x;
+	int r_y;
+	int b_x;
+	int b_y;
+	
+	cin >> n >> m;
+	for (int i=0; i<n;i++) {
+		cin >>p[i];
+		for (int j=0; j<m;j++) {
+			if(p[i][j]=='R') {
+				r_x=i;
+				r_y=j;
+				p[i][j]='.';
+			} else if(p[i][j]=='B') {
+				p[i][j]='.';
+			}
+		}
+	}
+	for (int i=0;i<4;i++) {
+		memset(visited,-1,sizeof(visited));
+		dfs(r_x,r_y,b_x,b_y,0,i);
+	}
+	
+	cout <<ans<<'\n';
+	return 0;
 }
