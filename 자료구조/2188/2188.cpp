7c24ee23575e401a74c6055cfc02f8506cb6ebe9
song <johyesong8686@gmail.com>
2021-03-16 00:45:58 +0900

    fix: 2188(축사배정) 풀이_재풀이 [o]

--- 자료구조/2188/2188.cpp
+++ 자료구조/2188/2188.cpp
@@ -1,84 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#define MAX 500
 using namespace std;
 
-vector <int> a[500];
-int c[500][500];
-int d[500];
-int f[500][500];
-int n,m;
-int result;
-void maxFlow(int start,int end){
-    
-    while(1){
-        fill(d,d+500,-1);
-        queue <int> q;
-        q.push(start);
-        d[start]=start;
-        while(!q.empty()){
-            
-            int x =q.front();
-            q.pop();
+vector<int> a[MAX];
+int c[MAX]; //왼쪽 그룹
+int f[MAX]; ///오른쪽 그그룹
+bool visited[MAX];
 
-            for(int i=0; i<a[x].size();i++){
-                int y= a[x][i];
-                if( c[x][y] - f[x][y] >0 &&d[y]==-1){
-                    q.push(y);
-                    d[y]=x;
-                    if(y==end)break;
-                }
-                
-            }
+int n, m;
+
+bool dfs(int start)
+{
+
+    visited[start] = true;
+    for (int i = 0; i < a[start].size(); i++)
+    {
+        int next = a[start][i];
+        if (f[next] == -1 || !visited[f[next]] && dfs(f[next]))
+        {
+            //매칭이 되지 않았거나
+            //매칭이 되었지만, 다른 점과 매칭 시킬수있다면
+            c[start] = next;
+            f[next] = start;
+            return true;
         }
-        
-        if(d[end] ==-1)break;
-        
-        int flow=1;
-        for(int i=end; i!=start ;i=d[i]){
-           f[d[i]][i]+=flow;
-           f[i][d[i]]-=flow;
-        }
-        result+=flow;
-        
     }
-    
+
+    return false;
 }
 
-int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
-  
-    cin>>n>>m;
- 
-    for(int i=1; i<=n;i++){
-       
-        a[0].push_back(i);
-        c[0][i]=1;
-    }
-    
-      for(int i=n+1; i<=n+m;i++){
-       
-        a[i].push_back(n+m+1);
-        c[i][n+m+1]=1;
-    }
-    
-    for(int i=1; i<=n;i++){
+int main()
+{
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
         int x;
-        cin >>x;
-        for(int j=0; j<x;j++){
+        cin >> x;
+        for (int j = 0; j < x; j++)
+        {
             int y;
-            cin >>y;
-            a[i].push_back(n+y);
-            c[i][n+y]=1;
-            
-            
+            cin >> y;
+            a[i].push_back(y - 1);
         }
     }
 
-  
-    
-     maxFlow(0,n+m+1);
-     cout <<result<<'\n';
- 
+    int ans = 0;
+
+    fill(c, c + MAX, -1);
+    fill(f, f + MAX, -1);
+    for (int i = 0; i < n; i++)
+    {
+        if (c[i] == -1)
+        {   //연결이 안되어있으면
+            //배열 초기화
+            fill(visited, visited + MAX, false);
+            if (dfs(i))
+                ans++;
+        }
+    }
+    cout << ans << '\n';
+
     return 0;
 }
