a358d33df840016f63386533eed90c151fda2320
song <johyesong8686@gmail.com>
2021-03-20 13:17:57 +0900

    fix: 14503(로봇청소기) 풀이 [o]

--- 구현/14503/14503.cpp
+++ 구현/14503/14503.cpp
@@ -2,48 +2,8 @@
 #include <cstring>
 using namespace std;
 
-int dx[] = {0, 1, 0, -1}; //북,동,남,서
-int dy[] = {-1, 0, 1, 0};
-
-int returnDir(int now)
-{ // 숫자에 따라 방향 지정
-    if (now == 0)
-    {
-        return 0;
-    }
-    else if (now == 1)
-    {
-        return 3;
-    }
-    else if (now == 2)
-    {
-        return 2;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-int nextDir(int now){
-    if (now == 0)
-    {
-        return 3;
-    }
-    else if (now == 1)
-    {
-        return 2;
-    }
-    else if (now == 2)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
+int dx[] = {-1, 0, 1, 0}; //북,동,남,서
+int dy[] = {0, 1, 0, -1};
 
 int main()
 {
@@ -66,47 +26,57 @@ int main()
         }
     }
     int ans = 0;
-    
 
     while (true)
     {
 
-        clean[x][y] = 1;
+        clean[x][y] = 1; //이동하면 청소
 
         bool go = false;
-        // cout <<x << " "<<y <<'\n';
+
+        int turn = d;
         for (int i = 0; i < 4; i++)
-        {   int turn=(returnDir(d) + i) % 4;
+        {
+
+            if (turn - 1 < 0)
+            {
+                turn = 3;
+            }
+            else
+            {
+                turn = turn - 1;
+            }
+
             int nx = x + dx[turn];
             int ny = y + dy[turn];
-            // cout <<"nx :"<<nx <<" "<<ny <<'\n';
-            if (nx>0 && nx <n-1&& ny>0 && ny <m-1 &&p[nx][ny]==0&&clean[nx][ny] == 0)
-            {   x=nx;
-                y=ny;
-                // cout <<"di"<<(i+1)%4<<'\n';
-                d=nextDir(turn);
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && p[nx][ny] == 0 && clean[nx][ny] == 0)
+            { //벽이아니고 청소하지 않았으면
+                x = nx;
+                y = ny;
+
+                d = turn;
                 go = true;
-              
-                //  cout <<"pass"<<'\n';
+
                 break;
-               
-               
             }
         }
-        
-        if(!go){
-            // cout <<"pass"<< "di"<<d<<'\n';
-           
-            int nx = x - dx[nextDir(d)];
-            int ny = y - dy[nextDir(d)];
-            if(nx>0 && nx <n-1&& ny>0 && ny <m-1){
-                x=nx;
-                y=ny;
-              
-            }else{
+
+        if (!go)
+        {
+
+            int nx = x - dx[d];
+            int ny = y - dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && p[nx][ny] == 0)
+            {
+                //끝점이 아니어도 벽일 경우에는 뒤로 못감
+                x = nx;
+                y = ny;
+            }
+            else
+            {
                 break;
             }
-          
         }
     }
 
@@ -114,13 +84,13 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            if(clean[i][j]){
+            if (clean[i][j])
+            {
                 ans++;
-            }  
+            }
         }
-       
     }
-    cout <<ans<<'\n';
+    cout << ans << '\n';
 
     return 0;
 }
