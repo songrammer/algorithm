a925cd301e2071e29f5ad2a0907547f5cc4a1dbe
song <johyesong8686@gmail.com>
2021-03-26 01:44:48 +0900

    fix: 16253(나무 재테크) 문제풀이 수정 [o]

--- 구현/16235/16235.cpp
+++ 구현/16235/16235.cpp
@@ -10,33 +10,27 @@ int p[15][15] = {
     0,
 };
 
+int dead[15][15] = {
+    0,
+};
 int dx[] = {0, 0, 1, -1, -1, -1, 1, 1};
 int dy[] = {-1, 1, 0, 0, -1, 1, 1, -1};
-struct Point
-{
-    int x, y, age, die;
-};
-
-bool cmp(const Point &u, const Point &v)
-{
-    return u.age < v.age;
-}
 
 int main()
 {
-
-    vector<Point> pq;
+    int ans = 0;
+    vector<int> pq[15][15]; //3차원 배열 좌표마다 백터값으로 넣기
 
     int n, m, k;
 
     cin >> n >> m >> k;
-   
 
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
             cin >> A[i][j];
+            p[i][j] = 5;
         }
     }
 
@@ -44,108 +38,90 @@ int main()
     {
         int x, y, value;
         cin >> x >> y >> value;
-        pq.push_back({x, y, value, 0});
+        pq[x][y].push_back(value);
+        ans++;
     }
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            p[i][j] = 5;
-        }
-    }
-    sort(pq.begin(), pq.end(), cmp);
 
     while (k--)
     {
 
-      
-        for (int i = 0; i < pq.size(); i++)
+        for (int i = 1; i <= n; i++)
         {
 
-            int x = pq[i].x;
-            int y = pq[i].y;
-            int age = pq[i].age;
-            if (pq[i].die)
-                continue;
-
-            if (p[x][y] - age >= 0)
+            for (int j = 1; j <= n; j++)
             {
-                p[x][y] -= age;
-                age += 1;
 
-                // cout <<"pass1"<<'\n';
-                pq[i].age += 1;
-            }
-            else
-            {
-                // cout <<"pass2"<<'\n';
-                pq[i].die = 1;
-                pq[i].age/=2;
+                sort(pq[i][j].begin(), pq[i][j].end()); //해당 좌표에 있는 나무들 정렬
+                p[i][j] += dead[i][j];                  //양분 추가(가을) 따로 빼도 상관무이다.
+                dead[i][j] = 0;                         //초기화
             }
         }
 
-        sort(pq.begin(), pq.end(), cmp);
-
-        for (int i = 0; i < pq.size(); i++)
+        for (int i = 1; i <= n; i++) //봄 
         {
 
-            int x = pq[i].x;
-            int y = pq[i].y;
-            int age = pq[i].age;
-            if (pq[i].die)
+            for (int j = 1; j <= n; j++)
             {
 
-                p[x][y] += age;
-            }
-        }
-
-        for (int i = 0; i < pq.size(); i++)
-        {
-
-            int x = pq[i].x;
-            int y = pq[i].y;
-            int age = pq[i].age;
-            if (pq[i].die) continue;
-
-            if (age % 5 == 0)
-            {
-
-                for (int j = 0; j < 8; j++)
+                for (int z = 0; z < pq[i][j].size(); z++)
                 {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-                    if (nx >= 1 && nx <= n && ny >= 1 && ny <= n)
+                    if (p[i][j] - pq[i][j][z] >= 0)
                     {
-                        pq.push_back({nx, ny, 1, 0});
+                        p[i][j] -= pq[i][j][z]; 
+                        pq[i][j][z] += 1;
+                    }
+                    else
+                    {
+
+                        for (int a = pq[i][j].size() - 1; a >= z; a--)
+                        {
+                            dead[i][j] += int(pq[i][j][a] / 2);
+                            pq[i][j].pop_back(); //부족한 지점이 있으면 그 점 포함 그 뒤로 모두 불가능하다.
+                            ans--;
+                        }
+                        break;
                     }
                 }
             }
-            
         }
-         sort(pq.begin(), pq.end(), cmp);
 
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++) //가을
         {
+
             for (int j = 1; j <= n; j++)
             {
+
+                for (int z = 0; z < pq[i][j].size(); z++)
+                {
+                    if (pq[i][j][z] % 5 == 0)
+                    {
+
+                        for (int a = 0; a < 8; a++)
+                        {
+                            int nx = i + dx[a];
+                            int ny = j + dy[a];
+                            if (nx >= 1 && nx <= n && ny >= 1 && ny <= n)
+                            {
+                                pq[nx][ny].push_back(1); 
+                                ans++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) //겨울 양분 더 해줌
+        {
+
+            for (int j = 1; j <= n; j++)
+            {
+
                 p[i][j] += A[i][j];
             }
         }
     }
 
-    int ans = 0;
-    for (int i = 0; i < pq.size(); i++)
-    {
-
-        int x = pq[i].x;
-        int y = pq[i].y;
-        int age = pq[i].age;
-        if (pq[i].die)
-            continue;
-
-        ans += 1;
-    }
     cout << ans << '\n';
 
     return 0;
