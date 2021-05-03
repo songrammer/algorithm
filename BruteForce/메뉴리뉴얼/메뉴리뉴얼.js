e73fb22ed903bfd1af2f99356a84e8e7fd61ea86
song <johyesong8686@gmail.com>
2021-05-03 22:50:28 +0900

    update: 메뉴리뉴얼 문제풀이 수정[o]

--- BruteForce/메뉴리뉴얼/메뉴리뉴얼.js
+++ BruteForce/메뉴리뉴얼/메뉴리뉴얼.js
@@ -7,7 +7,7 @@ let total = [];
 let ans = [];
 let max_num = [];
 
-for (var i = 1; i < 11; i++) {
+for (var i = 1; i < 15; i++) {
     total.push([]);
 }
 
@@ -16,7 +16,7 @@ function go(count, limit, str, start, or) {
         let include = 0;
         for (let i = 0; i < order.length; i++) {
             let result = true;
-            for (let k = 0; k < limit; k++) {
+            for (let k = 0; k < str.length; k++) {
                 if (!order[i].includes(str[k])) {
                     result = false;
                     break;
@@ -63,19 +63,18 @@ function solution(orders, course) {
     }
 
     search_al = search_al.split('').sort().join('')
+
     for (var i = 0; i < course.length; i++) {
         max_num.push(0);
         go(0, course[i], '', 0, i);
     }
-    ans.sort();
+
     for (var i = 0; i < course.length; i++) {
         if (max_num[i]) {
             for (var j = 0; j < total[course[i]].length; j++) {
                 if (total[course[i]][j][1] == max_num[i]) {
                     answer.push(total[course[i]][j][0]);
                 }
-
-
             }
 
         }
