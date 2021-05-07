bf6b37f9af56d4a793a05f758a52b80d2748b00a
song <johyesong8686@gmail.com>
2021-05-07 15:06:28 +0900

    update: 짝제거하기 [시간초과

--- 짝제거하기/짝제거하기.js
+++ 짝제거하기/짝제거하기.js
@@ -3,19 +3,22 @@ function solution(s) {
     let result;
     while (s.length) {
         result = 0;
+        let temp = '';
+
         for (var i = 0; i < s.length - 1; i++) {
             if (s[i] === s[i + 1]) {
-                s = s.slice(0, i) + s.slice(i + 2);
+                i += 1;
                 result = 1;
-                i = i - 1;
-
+            } else {
+                temp += s[i];
             }
         }
+
         if (!result) {
             break;
         }
 
-
+        s = temp;
     }
 
     return result;
